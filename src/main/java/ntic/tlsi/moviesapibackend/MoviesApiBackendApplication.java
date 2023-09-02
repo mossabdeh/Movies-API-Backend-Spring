7e4de7397b8015a1b42410daf5db9e1dae8a82a2
Mossab dehane <mossabdehane19@gmail.com>
2023-09-02 19:01:03 +0200

    first commit

---
+++ src/main/java/ntic/tlsi/moviesapibackend/MoviesApiBackendApplication.java
@@ -0,0 +1,13 @@
+package ntic.tlsi.moviesapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntic/tlsi/moviesapibackend/MoviesApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ntic.tlsi.moviesapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
